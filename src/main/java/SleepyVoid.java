e37dc3f77cc3f25db39cb907d17d0d047000b041
yx-tzzz <e1157327@u.nus.edu>
2025-01-31 01:20:49 +0800

    Added Level 0: Rename, Greet, Exit

---
+++ src/main/java/SleepyVoid.java
@@ -0,0 +1,16 @@
+public class SleepyVoid {
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "   /\\_/\\  \n" +
+                     "  / _ _ \\ \n" +
+                    " /       \\ \n" +
+                    "|   >o<   | \n" +
+                    "\\   _ _   / \n";
+        System.out.println("Hello... \n" + logo + "I'm void, what can I do for you?");
+        System.out.println("Bye, see you soonn");
+    }
+}
