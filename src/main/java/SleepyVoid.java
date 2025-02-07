9d77a2477183ffd00054a75f3c557a004dc7c5b6
yx-tzzz <e1157327@u.nus.edu>
2025-02-07 15:52:54 +0800

    Made minor adjustments and refactored code

--- src/main/java/SleepyVoid.java
+++ src/main/java/SleepyVoid.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 
 public class SleepyVoid {
     private static final String LEADING_SPACE = "                ";
-    private static final String TASK_COUNT_MESSAGE = "        Now there's %d pending task(s) in the list.";
+    private static final String TASK_COUNT_MESSAGE = "          Now there's %d pending task(s) in the list.";
 
-    public static void printTaskCount(int taskCount) {
+    public static void printTask(int taskCount, Task[] storedObjects) {
+        System.out.println(LEADING_SPACE + (taskCount) + ". " + storedObjects[taskCount-1].toString());
+    }
+
+    public static void printAddedTask(int taskCount, Task[] storedObjects) {
+        System.out.println("            " + "/ᐠ˵+ +˵マ added: " );
+        printTask(taskCount, storedObjects);
         System.out.printf((TASK_COUNT_MESSAGE) + "%n", taskCount);
     }
 
@@ -15,6 +21,7 @@ public class SleepyVoid {
                     "|   >o<   | \n" +
                     "\\   _ _   / \n";
         System.out.println("Hello... \n" + logo + "I'm void, what can I do for you..?");
+
         Scanner scanInputs = new Scanner(System.in);
         Task[] storedObjects = new Task[100];
         int taskCount = 0;
@@ -33,7 +40,7 @@ public class SleepyVoid {
                 case "list":
                     System.out.println("            /ᐠ˵= =˵マ: ");
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println(LEADING_SPACE + (i + 1) + ". " + storedObjects[i].toString());
+                        printTask(i + 1, storedObjects);
                     }
                     break;
 
@@ -42,43 +49,33 @@ public class SleepyVoid {
                     storedObjects[objectIndex].markAsDone();
                     System.out.print("            /ᐠ˵, ,˵マ ");
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(LEADING_SPACE + (objectIndex + 1) + storedObjects[objectIndex].toString());
+                    printTask(objectIndex + 1, storedObjects);
                     break;
 
                 case "todo":
                     storedObjects[taskCount] = new Todos(inputParts[1].trim());
                     taskCount++;
-                    System.out.println("            " +
-                            "/ᐠ˵+ +˵マ added: " );
-                    System.out.println(LEADING_SPACE + (taskCount) + "." + storedObjects[taskCount-1].toString());
-                    printTaskCount(taskCount);
+                    printAddedTask(taskCount, storedObjects);
                     break;
 
                 case "event":
                     String[] eventParts = inputParts[1].split(" /from | /to ",3);
                     storedObjects[taskCount] = new Events(eventParts[0], eventParts[1], eventParts[2]);
                     taskCount++;
-                    System.out.println("            " +
-                            "/ᐠ˵+ +˵マ added: " );
-                    System.out.println(LEADING_SPACE + (taskCount) + "." + storedObjects[taskCount-1].toString());
-                    printTaskCount(taskCount);
+                    printAddedTask(taskCount, storedObjects);
                     break;
 
                 case "deadline":
                     String[] deadlineParts = inputParts[1].split(" /by ", 2);
                     storedObjects[taskCount] = new Deadlines(deadlineParts[0], deadlineParts[1]);
                     taskCount++;
-                    System.out.println("            " + "/ᐠ˵+ +˵マ added: " );
-                    System.out.println(LEADING_SPACE + (taskCount) + "." + storedObjects[taskCount-1].toString());
-                    printTaskCount(taskCount);
+                    printAddedTask(taskCount, storedObjects);
                     break;
 
                 default:
-                    Task newTask = new Task(userInput);
-                    storedObjects[taskCount] = newTask;
+                    storedObjects[taskCount] = new Task(userInput);
                     taskCount++;
-                    System.out.println("            " + "/ᐠ˵+ +˵マ added: ");
-                    printTaskCount(taskCount);
+                    printAddedTask(taskCount, storedObjects);
             }
         }
 
